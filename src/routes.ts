b40a8068e5eafffbc3b115e59e3e63626e124cc1
Marco Tulio Vieira Martins <marcotulio_iub@hotmail.com>
2023-04-24 16:00:00 -0300

    Ajustes

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
 import { CreateProductController } from "./controllers/CreateProductController";
 import { CreateCategoryController } from "./controllers/CreateCategoryController";
+import { CreateProductCategoryController } from "./controllers/CreateProductCategoryController";
 
 const router = Router()
 
 const createProduct = new CreateProductController()
 const createCategory = new CreateCategoryController()
+const createProductCategory = new CreateProductCategoryController
 
 router.post("/product", createProduct.handle)
 router.post("/category", createCategory.handle)
+router.post("/productCategory", createProductCategory.handle)
 
 export { router }
